72b83c83526cafb640f438688c04ff3cc7c8b139
reichtergen <reichtergen.reinhard+github@gmail.com>
2022-03-22 18:02:39 +0300

    added important functions

--- sthread.c
+++ sthread.c
@@ -7,3 +7,53 @@
 #else
     #include <pthread.h>
 #endif
+
+typedef struct sthread_t {
+
+    #ifdef _WIN32
+        HANDLE thread;
+    #else
+        pthread_t thread;
+    #endif
+} sthread_t;
+
+
+void create_thread(sthread_t * th, void (*thread_function)(void*), void * args)  {
+
+    #ifdef _WIN32
+        th->thread = CreateThread(NULL, 0, thread_function, args, 0, NULL);
+    #else
+        pthread_create( th->thread, NULL, thread_function, args);
+    #endif   
+
+}
+
+void wait_thread(sthread_t * th) {
+
+    #ifdef _WIN32
+        WaitForSingleObject(th->thread, 0xFFFFFFFF);
+    #else
+        pthread_join( th->thread, NULL);
+    #endif   
+}
+
+void terminate_thread(sthread_t * th) {
+
+    #ifdef _WIN32
+        TerminateThread(th->thread, 0);
+        CloseHandle(th->thread);
+        th->thread = NULL;
+    #else
+        pthread_cancel(th->thread);
+        th->thread = NULL;
+    #endif   
+}
+
+void destroy_thread(sthread_t * th) {
+    #ifdef _WIN32
+        CloseHandle(th->thread);
+        th->thread = NULL;
+    #else
+        th->thread = NULL;
+    #endif   
+}
